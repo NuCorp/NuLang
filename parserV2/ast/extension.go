eba880256ee70a404329310d52047b67ec14c697
Florent Carrez <florent.carrez@epita.fr>
2024-04-02 21:23:46 +0200

    [feat: parserV2] typeDef + extension ast

---
+++ parserV2/ast/extension.go
@@ -0,0 +1,40 @@
+package ast
+
+import "github.com/DarkMiMolle/GTL/optional"
+
+type ExtensionDef struct {
+	ExtensionKw Keyword
+	Implem      optional.Value[ImplementInterface]
+
+	// either extension body or one line extension
+	Body     optional.Value[ExtensionBody]
+	OneLined optional.Value[ExtensionElement]
+}
+
+type ExtensionElement interface {
+	Ast
+	extensionElement()
+}
+
+type InitDef interface {
+	ExtensionElement
+}
+
+type CastDef interface {
+	ExtensionElement
+}
+
+type MethodDef interface {
+	ExtensionElement
+}
+
+type DeleterDef struct{}
+
+func (DeleterDef) extensionElement() {}
+
+type ExtensionBody struct {
+	Inits   []InitDef
+	Casts   []CastDef
+	Methods []MethodDef
+	Deleter optional.Value[DeleterDef]
+}
