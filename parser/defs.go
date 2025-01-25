4173b44b81da6e1d52a36f5badc14e8c6b0fe070
Florent Carrez <florent.carrez.fc@gmail.com>
2025-01-25 15:23:37 +0100

    defs struct

---
+++ parser/defs.go
@@ -0,0 +1,15 @@
+package parser
+
+import "github.com/DarkMiMolle/NuProjects/Nu-beta-1/parser/ast"
+
+type defs struct {
+	toplevel bool
+
+	typedef ParserOf[ast.TypeDef]
+	// castdef ParserOf[ast.CastDef]
+	// extensionDef ParserOf[ast.ExtensionDef]
+	vars ParserOf[[]ast.Var]
+	// consts ParserOf[[]ast.Const]
+	// funcs ParserOf[ast.FuncDef]
+	definedVars ParserOf[[]ast.Var]
+}
