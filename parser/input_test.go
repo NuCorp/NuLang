7b213ad7908431792a35be078f3fe33bbc2bf5c7
Florent Carrez <florent.carrez.fc@gmail.com>
2025-01-25 15:16:12 +0100

    add imports test (single import only)

---
+++ parser/input_test.go
@@ -0,0 +1,62 @@
+package parser
+
+import (
+	"testing"
+
+	tassert "github.com/stretchr/testify/assert"
+
+	"github.com/DarkMiMolle/NuProjects/Nu-beta-1/parser/ast"
+	"github.com/DarkMiMolle/NuProjects/Nu-beta-1/scan"
+)
+
+func Test_imports_Parse(t *testing.T) {
+	testcases := []struct {
+		name          string
+		code          string
+		withDotParser ParserOf[ast.DotIdent]
+
+		wantImports []ast.Import
+		wantErrors  Errors
+	}{
+		{
+			name: "single import same project",
+			code: `import pkg`,
+			withDotParser: parserFuncFor[ast.DotIdent](func(scanner scan.Scanner, errors *Errors) ast.DotIdent {
+				scanner.ConsumeTokenInfo()
+				return ast.DotIdent{"pkg"}
+			}),
+			wantImports: []ast.Import{
+				{
+					Package: ast.DotIdent{"pkg"},
+				},
+			},
+		},
+	}
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				single = singleImport{
+					dotIdent: tt.withDotParser,
+				}
+				project = projectImports{
+					single: single,
+				}
+				grouped = groupedImports{
+					single:  single,
+					project: project,
+				}
+				scanner = scan.Code(tt.code)
+				errors  = Errors{}
+
+				got = imports{
+					single:  single,
+					project: project,
+					grouped: grouped,
+				}.Parse(scanner, &errors)
+			)
+
+			tassert.Equal(t, tt.wantImports, got)
+			tassert.Equal(t, tt.wantErrors, errors)
+		})
+	}
+}
