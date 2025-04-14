e4aac236e37ca03474f8f157a1efe636bc441602
Florent Carrez <florent.carrez.fc@gmail.com>
2025-04-14 20:43:50 +0200

    init: rm comment

--- parser/init.go
+++ parser/init.go
@@ -137,18 +137,6 @@ func (si *simpleInitArgParser) Parse(s scan.Scanner, errors *Errors) simpleInitA
 	return simpleInitArg{simpleInitArg: &arg}
 }
 
-/*
-| {}
-| {EXPR}
-| {EXPR, Left}
-
-Left:
-| ø
-| NameArgBinding
-| NameArgBindingDestructured
-| IDENT
-*/
-
 func (i simpleInit) ContinueParsing(from ast.Type, s scan.Scanner, errors *Errors) ast.SimpleInitExpr {
 	assert(s.CurrentToken().IsOneOf(tokens.OBRAC, tokens.NOT, tokens.ASK))
 
@@ -189,54 +177,6 @@ func (i simpleInit) ContinueParsing(from ast.Type, s scan.Scanner, errors *Error
 	}
 
 	return init
-
-	s.ConsumeTokenInfo()
-
-	if s.CurrentToken() != tokens.STAR {
-		init.FromAs.Set(i.expr.Parse(s, errors))
-	}
-
-	if s.CurrentToken() == tokens.CBRAC {
-		s.ConsumeTokenInfo()
-		return init
-	}
-
-	switch {
-	case init.FromAs.HasValue() && s.CurrentToken() != tokens.COMMA:
-		errors.Set(s.CurrentPos(), "expected ',' to continue the init expression")
-		skipTo(s, tokens.COMMA)
-		return init
-	case init.FromAs.HasValue():
-		s.ConsumeTokenInfo()
-	default:
-	}
-
-	// TODO: it can be a bool argument too !
-	if s.CurrentToken() != tokens.STAR {
-		errors.Set(s.CurrentPos(), "init argument must be named")
-		skipTo(s, tokens.CBRAC)
-		return init
-	}
-
-	s.ConsumeTokenInfo()
-	ident := s.ConsumeTokenInfo()
-
-	if ident.Token() != tokens.IDENT {
-		errors.Set(s.CurrentPos(), "init argument must be named")
-		skipTo(s, tokens.CBRAC)
-		return init
-	}
-
-	if _, exists := init.Args[ident.Value().(string)]; exists {
-		errors.Set(s.CurrentPos(), "init argument already exists")
-		// continue
-	}
-
-	if s.CurrentToken() != tokens.COLON {
-
-	}
-
-	return ast.SimpleInitExpr{}
 }
 
 type initExpr struct {
@@ -247,7 +187,7 @@ type initExpr struct {
 
 func (i initExpr) isInterfaceInit(scanner scan.SharedScanner) bool {
 	if scanner.ConsumeToken().IsOneOf(tokens.CONST, tokens.SET) {
-		// TYPE { const Method ...
+		// TYPE:{ const Method ...
 		// or
 		// Type { set Method ...
 		return true
