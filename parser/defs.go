bb505769a1ce9c49a8099b719bdd9822f40202af
Florent Carrez <florent.carrez.fc@gmail.com>
2025-04-15 18:43:01 +0200

    parser/def: reorganize and better def selector

--- parser/defs.go
+++ parser/defs.go
@@ -10,69 +10,24 @@ import (
 	"github.com/NuCorp/NuLang/scan/tokens"
 )
 
-type defsParser struct {
-	defParser
-}
-type defParser struct {
-	toplevel bool
-
-	typedef      ParserOf[ast.TypeDef]
-	castdef      ParserOf[ast.CastDef]
-	extensionDef ParserOf[ast.ExtensionDef]
-	vars         ParserOf[[]ast.Var]
-	consts       ParserOf[[]ast.Const]
-	funcs        ParserOf[ast.FuncDef]
-	definedVars  ParserOf[[]ast.Var]
-
-	dotIdent ParserOf[ast.DotIdent]
-}
-
-func (d defsParser) Parse(s scan.Scanner, errors *Errors) []ast.Def {
-	var defs []ast.Def
-
-	for !s.IsEnded() {
-		switch s.CurrentToken() {
-		case tokens.VAR:
-			defs = append(defs, slices.Map(d.vars.Parse(s, errors), ast.Var.AsDef)...)
-		case tokens.CONST:
-			defs = append(defs, slices.Map(d.consts.Parse(s, errors), ast.Const.AsDef)...)
-		case tokens.FUNC:
-			if !d.toplevel {
-				errors.Set(s.CurrentPos(), "named function must be declared in top level (package) scope; use const+ lambda instead")
-			}
-
-			defs = append(defs, d.funcs.Parse(s, errors))
-		case tokens.TYPE:
-			parser := d.selectTypeParser(s.Clone(), errors)
-
-			if parser == nil {
-				break
-			}
-
-			defs = append(defs, parser.Parse(s, errors))
-		case tokens.IDENT:
-			if d.toplevel {
-				errors.Set(s.CurrentPos(), "ident can be used in top level (package) scope")
-				skipToEOI(s)
-				break
-			}
-		}
-
-		ignoreEoI(s)
+func defParserOf[F ast.Def](p ParserOf[F]) ParserOf[ast.Def] {
+	if p == nil {
+		return nil
 	}
 
-	return defs
-}
-
-func defParserOf[F ast.Def](p ParserOf[F]) ParserOf[ast.Def] {
 	return ConvertParserOf[F, ast.Def]{
 		Converter: ConverterFunc[F, ast.Def](F.AsDef),
 	}.Convert(p)
 }
 
-// selectTypeParser can return either ParserOf[ast.TypeDef], ParserOf[ast.CastDef] or ParserOf[ast.ExtensionDef]
-func (d defsParser) selectTypeParser(s scan.SharedScanner, errors *Errors) ParserOf[ast.Def] {
-	assert(s.ConsumeToken() == tokens.TYPE, "unexpected token %v", s.CurrentToken())
+type defSelector struct {
+	tokenSelection map[tokens.Token]ParserOf[ast.Def]
+
+	dotIdent ParserOf[ast.DotIdent]
+}
+
+func (d defSelector) selectParser(s scan.SharedScanner, errors *Errors) ParserOf[ast.Def] {
+	assert(s.ConsumeToken() == tokens.TYPE, "unexpected token; expected `type`")
 
 	if s.CurrentToken() != tokens.IDENT {
 		errors.Set(s.CurrentPos(), "expected identifier after `type` keyword in type definition related")
@@ -85,19 +40,14 @@ func (d defsParser) selectTypeParser(s scan.SharedScanner, errors *Errors) Parse
 
 	d.dotIdent.Parse(s, &errs)
 
-	switch tokInfo := s.CurrentTokenInfo(); tokInfo.Token() {
-	case tokens.ASSIGN:
-		return defParserOf[ast.TypeDef](d.typedef)
-	case tokens.PLUS_ASSIGN:
-		return defParserOf[ast.ExtensionDef](d.extensionDef)
-	case tokens.AS:
-		return defParserOf[ast.CastDef](d.castdef)
-	default:
+	parser, ok := d.tokenSelection[s.CurrentToken()]
+
+	if !ok {
 		errors.Set(
-			tokInfo.FromPos(),
+			s.CurrentTokenInfo().FromPos(),
 			fmt.Sprintf(
 				"impossible to make a type definition with operator `%v`",
-				tokInfo.Token(),
+				s.CurrentTokenInfo().Token(),
 			),
 		)
 
@@ -106,4 +56,79 @@ func (d defsParser) selectTypeParser(s scan.SharedScanner, errors *Errors) Parse
 
 		return nil
 	}
+
+	return parser
+}
+
+type topLevelDefParser struct {
+	vars   ParserOf[[]ast.Var]
+	consts ParserOf[[]ast.Const]
+	funcs  ParserOf[ast.FuncDef]
+
+	typeDef      ParserOf[ast.TypeDef]
+	castDef      ParserOf[ast.CastDef]
+	extensionDef ParserOf[ast.ExtensionDef]
+
+	dotIdent ParserOf[ast.DotIdent]
+}
+
+func (d topLevelDefParser) Parse(s scan.Scanner, errors *Errors) []ast.Def {
+	var (
+		defs []ast.Def
+
+		typeSelector = defSelector{
+			tokenSelection: map[tokens.Token]ParserOf[ast.Def]{
+				tokens.ASSIGN:      defParserOf(d.typeDef),
+				tokens.PLUS_ASSIGN: defParserOf(d.extensionDef),
+				tokens.AS:          defParserOf(d.castDef),
+			},
+
+			dotIdent: d.dotIdent,
+		}
+	)
+
+	for !s.IsEnded() {
+		switch s.CurrentToken() {
+		case tokens.VAR:
+			defs = append(defs, slices.Map(d.vars.Parse(s, errors), ast.Var.AsDef)...)
+		case tokens.CONST:
+			defs = append(defs, slices.Map(d.consts.Parse(s, errors), ast.Const.AsDef)...)
+		case tokens.FUNC:
+			defs = append(defs, d.funcs.Parse(s, errors))
+		case tokens.TYPE:
+			parser := typeSelector.selectParser(s.Clone(), errors)
+
+			if parser == nil {
+				break
+			}
+
+			defs = append(defs, parser.Parse(s, errors))
+		default:
+			// error
+		}
+
+		ignoreEoI(s)
+	}
+
+	return defs
+}
+
+type typeLevelDefParser struct{}
+
+func (d typeLevelDefParser) Parse(s scan.Scanner, errors *Errors) []ast.Def {
+	panic("implement me")
+}
+
+type funcLevelDefParser struct {
+	typedef     ParserOf[ast.TypeDef]
+	castdef     ParserOf[ast.CastDef]
+	vars        ParserOf[[]ast.Var]
+	consts      ParserOf[[]ast.Const]
+	definedVars ParserOf[[]ast.Var]
+
+	dotIdent ParserOf[ast.DotIdent]
+}
+
+func (d funcLevelDefParser) Parse(s scan.Scanner, errors *Errors) []ast.Def {
+	panic("implement me")
 }
