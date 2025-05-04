dc814e530ab9913c35f0f41d5f9d5482700102c0
Florent Carrez <florent.carrez.fc@gmail.com>
2025-05-04 22:04:08 +0200

    ast: use contained elem in tuple/array

--- parser/ast/expr.go
+++ parser/ast/expr.go
@@ -1,18 +1,59 @@
 package ast
 
+import "github.com/NuCorp/NuLang/utils/set"
+
 type Expr interface {
 	AsExpr() Expr
 }
 
 func (d DotIdent) AsExpr() Expr { return d }
 
-type TupleExpr []Expr
+type Elem = OrderedContainedElem
+type DestructuredOrderedElem = DestructuredContainedElem[Elem]
+
+// ListElem allows EXPR and EXPR...
+type ListElem = containedElem[Elem, noneContainedElem, DestructuredOrderedElem]
+
+func NewListElem(expr Expr) ListElem {
+	return ListElem{
+		Ordered: &OrderedContainedElem{
+			Expr: expr,
+		},
+	}
+}
+
+func DestructuredListElem(l ListElem) ListElem {
+	if l.Ordered == nil || l.Destructured != nil {
+		return l
+	}
+
+	l.Destructured = &DestructuredOrderedElem{
+		ContainedElem: *l.Ordered,
+	}
+
+	l.Ordered = nil
+
+	return l
+}
+
+// TupleExpr can be : (EXPR, EXPR...)
+type TupleExpr []ListElem
 
 func (t TupleExpr) AsExpr() Expr { return t }
 
-type ArrayExpr []Expr
+// ArrayExpr can be : [EXPR, EXPR...]
+type ArrayExpr []ListElem
 
 func (a ArrayExpr) AsExpr() Expr { return a }
+
+type NamedElem = NamedContainedElem
+type DestructuredNamedElem = DestructuredContainedElem[NamedElem]
+
+type DictExpr struct {
+	Keys         []Expr
+	Values       map[int]Expr
+	Destructured set.Set[int]
+}
 
 type AsTypeExpr struct {
 	Forced bool
