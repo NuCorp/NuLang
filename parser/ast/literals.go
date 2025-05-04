e7b4d2fd219b1b4b203a430ac147d254a549d1a0
Florent Carrez <florent.carrez.fc@gmail.com>
2025-05-04 22:02:34 +0200

    ast: add StringExpr

--- parser/ast/literals.go
+++ parser/ast/literals.go
@@ -9,3 +9,8 @@ type IntExpr int
 
 func (i IntExpr) AsExpr() Expr      { return i }
 func (i IntExpr) LiteralValue() any { return int(i) }
+
+type StringExpr string
+
+func (s StringExpr) AsExpr() Expr      { return s }
+func (s StringExpr) LiteralValue() any { return string(s) }
