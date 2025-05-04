d77c0d82b3432fa1c3cf5b615e5ec34f0e27356f
Florent Carrez <florent.carrez.fc@gmail.com>
2025-05-04 22:03:46 +0200

    ast: rename function file and use contained elem

---
+++ parser/ast/functions.go
@@ -0,0 +1,45 @@
+package ast
+
+import "github.com/LicorneSharing/GTL/optional"
+
+type CallableFunc interface {
+	FuncID() string
+}
+
+type FuncType struct {
+	Arguments   []Type
+	NamedArgs   map[int]string
+	VariadicArg optional.Value[int]
+	ReturnType  Type
+}
+
+type FuncDef struct {
+	Name   string
+	Header FuncType
+	Body   any
+}
+
+type FuncExpr struct {
+	Header FuncType
+	Body   any
+}
+
+func (f FuncExpr) AsExpr() Expr { return f }
+func (FuncExpr) FuncID() string { return "func:func" }
+
+func (DotIdent) FuncID() string { return "func:named" }
+
+type (
+	OrderedArgElem      = OrderedContainedElem
+	NamedArgElem        = NamedContainedElem
+	DestructuredArgElem = DestructuredContainedElem[ContainedElem]
+
+	ArgElem = containedElem[OrderedArgElem, NamedArgElem, DestructuredArgElem]
+)
+
+type FuncCall struct {
+	From CallableFunc
+	Args []ArgElem
+}
+
+func (f FuncCall) AsExpr() Expr { return f }
