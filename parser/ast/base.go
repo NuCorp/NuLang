397787157b882c9e34d77d3cb8f9c2cec3edfd3b
Florent Carrez <florent.carrez.fc@gmail.com>
2025-05-04 22:02:01 +0200

    ast.DotIdent: add method First/Last

--- parser/ast/base.go
+++ parser/ast/base.go
@@ -7,3 +7,15 @@ type DotIdent []string
 func (d DotIdent) Pack() string {
 	return strings.Join(d, ".")
 }
+
+func (d DotIdent) First() string {
+	if d[0] == "" {
+		return "self"
+	}
+
+	return d[0]
+}
+
+func (d DotIdent) Last() string {
+	return d[len(d)-1]
+}
